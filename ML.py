40eb1481c6e7c1b2baf45c636082c5adae0e6f53
louiewhw <louiewhw@gmail.com>
2020-01-21 01:43:36 -0800

    added ML model

--- ML.py
+++ ML.py
@@ -1,9 +1,44 @@
 from sklearn.model_selection import cross_val_score
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
 import tensorflow as tf
+import pickle
 
 
-mnist = tf.keras.datasets.mnist.load_data()
+(X_train, y_train), (X_test, y_test) = tf.keras.datasets.mnist.load_data()
+
+X_train = X_train.reshape(60000, 784)
+
+X_test = X_test.reshape(10000, 784)
+
+dt = DecisionTreeClassifier()
+
+rf = RandomForestClassifier()
+svc = SVC()
+
+print('Training {}'.format('Random Forest'))
+rf.fit(X_train, y_train)
+
+print('Training {}'.format('Decision Tree'))
+dt.fit(X_train, y_train)
 
 
-print(mnist)
+print('Training {}'.format('SVC'))
+svc.fit(X_train, y_train)
+
+rf_score = rf.score(X_test, y_test)
+print('{} Score: {}'.format('Random Forest', rf_score))
+
+dt_score = dt.score(X_test, y_test)
+print('{} Score: {}'.format('Decision Tree', dt_score))
+
+svc_score = svc.score(X_test, y_test)
+print('{} Score: {}'.format('SVC', svc_score))
+
+print()
+
+pickle.dump((rf, rf_score), open('RandomForest.pkl','wb'))
+pickle.dump((dt, dt_score), open('DecisionTree.pkl','wb'))
+pickle.dump((svc, svc_score), open('SVC.pkl','wb'))
+
--- app.py
+++ app.py
@@ -1,12 +1,52 @@
 from flask import Flask, jsonify, request, render_template
+import pickle
+import base64
+import re
+from io import BytesIO
+from PIL import Image
+import numpy as np
+
+dt, dt_score = pickle.load(open('DecisionTree.pkl', 'rb'))
+rf, rf_score = pickle.load(open('RandomForest.pkl', 'rb'))
+
+svc, svc_score = pickle.load(open('SVC.pkl', 'rb'))
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        svc_predict = get_predict()
+        
 
+    else:
+        svc_predict = '?'
+    return render_template('index.html', dt_prediction=svc_predict)
+
+
+# @app.route("/predict", methods=['POST'])
+def get_predict():
+
+    req = request.get_json()[22:]
+    imgdata = base64.b64decode(req)
+
+    img = Image.open(BytesIO(imgdata)).convert('L')
+    img = img.resize((28, 28))
+    img = np.array(img).reshape(1,784)
+
+    svc_predict = predict(img)
+    print(svc_predict)
+    return svc_predict
+    
+
+
+
+def predict(arr):
+    svc_predict = svc.predict(arr)
+    
+    return svc_predict
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.config["TEMPLATES_AUTO_RELOAD"] = True
+    app.run()
